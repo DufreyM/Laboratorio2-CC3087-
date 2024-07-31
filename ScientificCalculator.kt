105ad55aef8d2e4034b2b385dbaab073d062e2c8
Leonardo Mejía <leomejia646@gmail.com>
2024-07-30 18:34:52 -0600

    Comentarios sobre el código

--- ScientificCalculator.kt
+++ ScientificCalculator.kt
@@ -3,7 +3,10 @@ import kotlin.math.*
 
 class ScientificCalculator {
 
+    //Función que evalua la expresión del usuario y la tokeniza. 
+
     fun evaluate(expression: String): Double {
+    //Se reemplazan los espacios para ir creando los tokens.
         val tokens = expression.replace(" ", "").toCharArray()
         val values = Stack<Double>()
         val ops = Stack<Char>()
@@ -45,21 +48,25 @@ class ScientificCalculator {
         return values.pop()
     }
 
+    //Indica que operación se debe manejar según lo que indica el token, principalmente por la raiz y la potencia que no usan el mismo signo. 
     private fun applyOp(op: Char, b: Double, a: Double): Double {
         return when (op) {
             '+' -> a + b
             '-' -> a - b
             '*' -> a * b
             '/' -> {
+                //Manejo del error de la división sobre 0. 
                 if (b == 0.0) throw UnsupportedOperationException("Cannot divide by zero")
                 a / b
             }
             '^' -> a.pow(b)
             '√' -> sqrt(a)
+            //Manejo del error si se ingresa algún signo diferente. 
             else -> throw UnsupportedOperationException("Unsupported operation")
         }
     }
-
+    //Función para prioridad entre las operaciones
+    
     private fun hasPrecedence(op1: Char, op2: Char): Boolean {
         if (op2 == '(' || op2 == ')') return false
         if ((op1 == '*' || op1 == '/' || op1 == '^' || op1 == '√') && (op2 == '+' || op2 == '-')) return false
